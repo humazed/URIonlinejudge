0f4440901514b62b89ac7be158669f3444642874
Ibrahim Eid <humazed@gmail.com>
2015-06-22 23:13:20 +0200

    1005

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main() {
 
 
-    int a, b;
+    float a, b;
 
-    cin >> a >> b;
+    cin >> setprecision(1) >> a >> b;
 
-    cout << "PROD = " << a * b << endl;
+    cout << "MEDIA = " << fixed << setprecision(5) << (a * 3.5 + b * 7.5) / 11 << endl;
 
     return 0;
 }
