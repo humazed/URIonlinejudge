e501e6582983d9d9de0663b59e49f1b0170f5886
Ibrahim Eid <humazed@gmail.com>
2015-06-24 13:18:22 +0200

    1017

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
 
 
-    int a;
-    cin >> a;
+    int a, b;
+    cin >> a >> b;
 
-    cout << a * 2 << " minutos" << endl;
+    cout << fixed << setprecision(3) << a * b / 12.0 << endl;
 
     return 0;
 }
