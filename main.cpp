6c5cdbbaa3371532c5bf2ed8872a6e0079b1a6b3
Ibrahim Eid <humazed@gmail.com>
2015-06-24 13:42:26 +0200

    1018

--- main.cpp
+++ main.cpp
@@ -6,10 +6,26 @@ using namespace std;
 int main() {
 
 
-    int a, b;
-    cin >> a >> b;
+    int a;
+    cin >> a;
 
-    cout << fixed << setprecision(3) << a * b / 12.0 << endl;
+    int s100, s50, s20, s10, s5, s2;
+
+    s100 = a % 100;
+    s50 = s100 % 50;
+    s20 = s50 % 20;
+    s10 = s20 % 10;
+    s5 = s10 % 5;
+    s2 = s5 % 2;
+
+    cout << a << endl;
+    cout << a / 100 << " nota(s) de R$ 100,00" << endl;
+    cout << s100 / 50 << " nota(s) de R$ 50,00" << endl;
+    cout << s50 / 20 << " nota(s) de R$ 20,00" << endl;
+    cout << s20 / 10 << " nota(s) de R$ 10,00" << endl;
+    cout << s10 / 5 << " nota(s) de R$ 5,00" << endl;
+    cout << s5 / 2 << " nota(s) de R$ 2,00" << endl;
+    cout << s2 / 1 << " nota(s) de R$ 1,00" << endl;
 
     return 0;
 }
