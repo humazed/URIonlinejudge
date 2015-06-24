0defc48e32c866cc4ffc2dc2803fe43f6c0f3ef9
Ibrahim Eid <humazed@gmail.com>
2015-06-24 14:19:05 +0200

    1019

--- main.cpp
+++ main.cpp
@@ -5,27 +5,16 @@ using namespace std;
 
 int main() {
 
-
     int a;
     cin >> a;
 
-    int s100, s50, s20, s10, s5, s2;
+    int h, m;
 
-    s100 = a % 100;
-    s50 = s100 % 50;
-    s20 = s50 % 20;
-    s10 = s20 % 10;
-    s5 = s10 % 5;
-    s2 = s5 % 2;
+    h = a % (60 * 60);
+    m = h % 60;
 
-    cout << a << endl;
-    cout << a / 100 << " nota(s) de R$ 100,00" << endl;
-    cout << s100 / 50 << " nota(s) de R$ 50,00" << endl;
-    cout << s50 / 20 << " nota(s) de R$ 20,00" << endl;
-    cout << s20 / 10 << " nota(s) de R$ 10,00" << endl;
-    cout << s10 / 5 << " nota(s) de R$ 5,00" << endl;
-    cout << s5 / 2 << " nota(s) de R$ 2,00" << endl;
-    cout << s2 / 1 << " nota(s) de R$ 1,00" << endl;
+
+    cout << a / (60 * 60) << ":" << h / 60 << ":" << m << endl;
 
     return 0;
 }
